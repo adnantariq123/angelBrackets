b7c616383b03b29cde030aecefff257a613b76c7
adnantariq123 <ahmedadnan123@hotmail.com>
2025-10-17 10:24:50 -0400

    adding basic hello world

---
+++ my-first-app/src/app/hello/hello.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  imports: [],
+  templateUrl: './hello.component.html',
+  styleUrl: './hello.component.scss'
+})
+export class HelloComponent {
+    name = 'Adi';
+
+
+    changeName() {
+        this.name = 'Motto';
+    }
+}
