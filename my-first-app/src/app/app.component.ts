cbe55873da16ec9ccad599815ee536d7a4ac25bd
adnantariq123 <ahmedadnan123@hotmail.com>
2025-10-20 09:40:16 -0400

    fixed routing was missing RouterModule

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
