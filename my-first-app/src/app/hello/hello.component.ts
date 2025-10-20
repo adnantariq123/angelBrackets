c78dd75d3b01b93036d28607fcbd22fe423059ac
adnantariq123 <ahmedadnan123@hotmail.com>
2025-10-20 10:51:40 -0400

    added @if and ng-container to achive the same toggle functionality like *ngIf

--- my-first-app/src/app/hello/hello.component.ts
+++ my-first-app/src/app/hello/hello.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+// Import FormsModule to use ngModel for two-way data binding, and ngForm, and ngModelGroup
+import { CommonModule } from '@angular/common'; 
+// Import CommonModule to use structural directives like *ngIf, *ngFor, etc.
+  
 @Component({
   selector: 'app-hello',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './hello.component.html',
   styleUrl: './hello.component.scss'
 })
 export class HelloComponent {
     name = 'Adi';
+    SecondName = 'Adnan';
+    lastName = "Tariq";
 
 
     changeName() {
