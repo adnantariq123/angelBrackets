027c2a8549dbc6c5e8313ac0a45c787a4af42ede
adnantariq123 <ahmedadnan123@hotmail.com>
2025-10-24 11:32:19 -0400

    a LOT of comments in the looping.components.ts file BUT ALL VERY VALID

--- my-first-app/src/app/looping/looping.component.ts
+++ my-first-app/src/app/looping/looping.component.ts
@@ -1,30 +1,75 @@
 import { Component, OnInit  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+// httpClient - Angular service for making HTTP calls
+
+
+type userType = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: number;
+  };
+  phone: string;
+  website: string;
+  company: {
+      name: string;
+      catchPhrase: string;
+      bs: string;
+    }
+}
+type userTypeArray = userType[] | null
+
+// Type declarations (type userType) cannot be placed between the decorator and the class — 
+// ALWAYS put them above the decorator or in a separate file.
 
 @Component({
   selector: 'app-looping',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './looping.component.html',
   styleUrl: './looping.component.scss'
 })
 
-
 export class LoopingComponent implements OnInit {
-  FakeData: any = null;
+ // implements OnInit - This tells typeScript this class(LoopingComponent) MUST have a ngOnInit() used inside it 
+ // or else it will trow a compile-time error. We could have written the class with implements OnInit as well.
 
-  constructor(private http: HttpClient) {}
 
-  ngOnInit() { // ngOnInit works as Angular’s “componentDidMount / useEffect([] in React.js
-    this.http.get('https://jsonplaceholder.typicode.com/users')
+  FakeData: userTypeArray = null;
+
+  constructor(private Jimmy: HttpClient) {}
+  // private Jimmy: HttpClient - give us an instance of HttpClient from your injector... and I named it Jimmy for Demo learning.
+  // private - Makes it a class property, and you can name it whatever like Jimmy, BUT to be more profesional name it http next time :)
+
+  
+  // ngOnInit works as “componentDidMount / useEffect([] in React.js
+  ngOnInit() { 
+    this.Jimmy.get<userTypeArray>('https://jsonplaceholder.typicode.com/users')
+    // this.Jimmy.get () returns an Observable — a lazy data stream that only runs when you subscribe
+
       .subscribe({
-        next: (res) => {
+        // .subscribe actually starts the async operation (the HTTP request)
+        next: (res) => { // next is called when a new value arrives
           this.FakeData = res;
           console.log('Fetched data:', this.FakeData);
         },
-        error: (err) => {
+        error: (err) => { // error is called when something goes wrong
           console.error('Error fetching data:', err);
-        }
+        },
+
+        // complete: Called when the stream finishes
+
+        // try/catch/finally → handles a single synchronous or awaited async operation.
+        // subscribe() → can handle a stream of multiple asynchronous events (like live updates, websockets, etc.)
+        // subscribe is used for streaming.
       });
   }
 }
+
+
